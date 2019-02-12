aaae47841bf193f5cbded1f1ac54c333888a7d18
willyawan16 <44739889+willyawan16@users.noreply.github.com>
2019-02-12 18:53:10 +0700

    users path fixed

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,7 +4,7 @@ import Dashboard from '../screens/Dashboard';
 import MCourses from '../screens/MCourses';
 import AddCourses from '../screens/AddCourses';
 import Teacher from '../screens/Teacher';
-import users from '../screens/users'
+import Users from '../screens/Users'
 
 
 export default class Navigation extends Component{
@@ -12,7 +12,7 @@ export default class Navigation extends Component{
         return (
             <Switch>
                 <Route exact path='/' component={Dashboard}/>
-                <Route path='/users' component={users}/>
+                <Route path='/users' component={Users}/>
                 <Route path='/teacher' component={Teacher}/>
                 <Route path='/courses' component={MCourses}/>
                 <Route path='/addcourses' component={AddCourses}/>
