180586c7eb8dea3471150bf1029d046e819d07c2
willyawan16 <44739889+willyawan16@users.noreply.github.com>
2019-02-17 12:07:10 +0700

    tes2

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,6 +5,7 @@ import MCourses from '../screens/MCourses';
 import AddCourses from '../screens/AddCourses';
 import Teacher from '../screens/Teacher';
 import Users from '../screens/Users';
+import AddUsers from '../screens/AddUsers'
 
 
 export default class Navigation extends Component{
@@ -16,7 +17,7 @@ export default class Navigation extends Component{
                 <Route path='/teacher' component={Teacher}/>
                 <Route path='/courses' component={MCourses}/>
                 <Route path='/addcourses' component={AddCourses}/>
-                {/* <Route path='/addusers' component={AddUsers}/> */}
+                <Route path='/addusers' component={AddUsers}/>
                 {/* <Route path='/attendence' component={attendence}/>
                 <Route path='/payment' component={payment}/>
                 <Route path='/feedback' component={feedback}/> */}
