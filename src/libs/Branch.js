fc8096f92bbcaabfa1820c60f27ca428c56a5afc
Junior Tanaya <juniortanaya@gmail.com>
2019-03-03 14:13:13 +0700

    add Branch

--- src/libs/Branch.js
+++ src/libs/Branch.js
@@ -1,20 +1,24 @@
 import firebase from 'firebase';
 import { get } from 'https';
 
-export default class Branch {
-    getBranch = (callback) => {
-        const db = firebase.firestore();
-            db.settings ({
-                timestampsInSnapshots : true
+export function getBranch(callback) {
+    const db = firebase.firestore();
+        db.settings({
+        timestampsInSnapshots: true
+    });
+    db.collection("branch").get()
+    .then((snapshot) => {
+        let branch_list = []
+        snapshot.forEach((doc) => {
+            // console.log(doc.id, '=>', doc.data());
+            branch_list.push({
+                id: doc.id,
+                data: doc.data()
             });
-        db.collection('users').get()
-            .then((snapshot) => {
-            snapshot.forEach((doc) => {
-                console.log(doc.id, '=>', doc.data());
-            });
-        })
-        .catch((err) => {
-            console.log('Error getting documents', err);
         });
-    }
-}
+        callback(branch_list);
+    })
+    .catch((err) => {
+        console.log('Error getting documents', err);
+    });
+};
