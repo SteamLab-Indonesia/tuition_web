d0843e303bb0865d285d894b945008e5428dde0b
willyawan16 <44739889+willyawan16@users.noreply.github.com>
2019-03-03 14:14:36 +0700

    get org list

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import React, { Component } from 'react';
 import MyHeader from './components/MyHeader';
 //import Dashboard from './screens/Dashboard';
 import Navigation from './components/Navigation';
-import {initFirebase} from './libs/firebase'
+import {initFirebase} from './libs/firebase';
+import {getOrganization} from './libs/Organization';
+// var Organization = require('./libs/Organization');
 
 initFirebase();
+getOrganization((list) => {
+  console.log(list);
+})
 class App extends Component {
   render() {
     return (
