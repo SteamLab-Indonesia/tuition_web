04e88671cd87920a141a6c18674787fdc6fb55f9
hs <hsmok81@gmail.com>
2019-03-31 12:04:49 +0700

    set screen height as window height

--- src/styles/index.js
+++ src/styles/index.js
@@ -37,10 +37,12 @@ export const theme = createMuiTheme({
 export const ScreenStyle = {
     paddingTop: '120px',
     paddingLeft: '240px',
-    backgroundColor: 'cornflowerblue'   
+    backgroundColor: 'cornflowerblue',
+    height: window.innerHeight   
 };
 
 export const ScreenCloseStyle = {
     paddingTop: '120px',
-    backgroundColor: 'cornflowerblue'    
+    backgroundColor: 'cornflowerblue',
+    height: window.innerHeight    
 };
