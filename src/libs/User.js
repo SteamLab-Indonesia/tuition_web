ab428dd29aeab3b39d40d2ebc056b766a5fa2e31
hs <hsmok81@gmail.com>
2019-03-31 12:57:27 +0700

    Improving User Library

--- src/libs/User.js
+++ src/libs/User.js
@@ -1,5 +1,54 @@
 import firebase from 'firebase';
 
+export const GENDER = {
+    MALE: 'm',
+    FEMALE: 'f'
+};
+
+export class User {
+
+    name = '';
+    email = '';
+    username = '';
+    birthday = '';
+    password = '';
+    phone = '';
+    address = '';
+    school = '';
+    gender = GENDER.MALE;
+
+    constructor(user)
+    {
+        if (user)
+        {
+            this.name = user.name;
+            this.email = user.email;
+            this.username = user.username;
+            this.birthday = user.birthday;
+            this.password = user.password;
+            this.phone = user.phone;
+            this.address = user.address;
+            this.school = user.school;
+            this.gender = user.gender;    
+        }
+    }
+
+    toJson = () => {
+        return {
+            name: this.name,
+            email: this.email,
+            username: this.username,
+            birthday: this.birthday,
+            password: this.password,
+            phone: this.phone,
+            address: this.address,
+            school: this.school,
+            gender: this.gender
+        }
+
+    }
+}
+
 export function getUser(callback) {
     const db = firebase.firestore();
     db.collection("user").get()
@@ -18,3 +67,9 @@ export function getUser(callback) {
         console.log('Error getting documents', err);
     });
 };
+
+export function addUser(user)
+{
+    const db = firebase.firestore();
+    db.collection('user').add(user.toJson());      
+}
